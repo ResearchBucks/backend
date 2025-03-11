b40dca696b928410318d0a7ec0d6ac58e97d1f8e
Thakshila-Bandara <y.m.t.d.bandara@gmail.com>
2025-03-11 22:27:33 +0530

    feat: added temporary basic authentication and modified user registration

--- UserService-API/src/main/java/org/researchbucks/controller/UserController.java
+++ UserService-API/src/main/java/org/researchbucks/controller/UserController.java
@@ -6,6 +6,7 @@ import org.researchbucks.dto.UserRegDto;
 import org.researchbucks.repository.UserRepository;
 import org.researchbucks.service.UserService;
 import org.researchbucks.util.CommonMessages;
+import org.researchbucks.util.InputValidationUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,16 +23,25 @@ public class UserController {
 
     @PostMapping("/register")
     public ResponseEntity<ResponseDto> registerRespondent(@RequestBody UserRegDto userRegDto){
-        if(userRepository.existsRespondentByEmail(userRegDto.getEmail())){
-            log.error(CommonMessages.RESPONDENT_EMAIL_ALREADY_EXISTS);
+        if(userRepository.existsRespondentByEmail(userRegDto.getEmail()) || userRepository.existsRespondentByNic(userRegDto.getNic())){
+            log.error(CommonMessages.RESPONDENT_ALREADY_EXISTS);
             return ResponseEntity.badRequest().body(
                     ResponseDto.builder()
-                            .message(CommonMessages.RESPONDENT_EMAIL_ALREADY_EXISTS)
+                            .message(CommonMessages.RESPONDENT_ALREADY_EXISTS)
                             .status(CommonMessages.RESPONSE_DTO_FAILED)
                             .data(userRegDto)
                             .build()
             );
-        }else {
+        } else if (!InputValidationUtil.isValidNic(userRegDto.getNic())) {
+            log.error(CommonMessages.INVALID_NIC);
+            return ResponseEntity.badRequest().body(
+                    ResponseDto.builder()
+                            .message(CommonMessages.INVALID_NIC)
+                            .status(CommonMessages.RESPONSE_DTO_FAILED)
+                            .data(userRegDto)
+                            .build()
+            );
+        } else {
             return ResponseEntity.ok().body(
                     userService.registerRespondent(userRegDto)
             );
--- UserService-API/src/main/java/org/researchbucks/repository/UserRepository.java
+++ UserService-API/src/main/java/org/researchbucks/repository/UserRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<Respondent, Long> {
     boolean existsRespondentByEmail(String email);
+
+    boolean existsRespondentByNic(String nic);
 }

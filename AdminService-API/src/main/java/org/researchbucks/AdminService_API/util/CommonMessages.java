aab4de1d1e6f0603fc8cb5e178dfbf3300082f15
Thakshila-Bandara <y.m.t.d.bandara@gmail.com>
2025-04-29 22:42:04 +0530

    feat: added required classes

---
+++ AdminService-API/src/main/java/org/researchbucks/AdminService_API/util/CommonMessages.java
@@ -0,0 +1,7 @@
+package org.researchbucks.AdminService_API.util;
+
+public class CommonMessages {
+
+    public static final String RESPONSE_DTO_SUCCESS = "Success";
+    public static final String RESPONSE_DTO_FAILED = "Failed";
+}

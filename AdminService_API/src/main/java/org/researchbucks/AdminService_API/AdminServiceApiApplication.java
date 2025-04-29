4f57034ab81a8143796052a105039764c8346f5e
Thakshila-Bandara <y.m.t.d.bandara@gmail.com>
2025-04-29 22:15:51 +0530

    feat: Added AdminService_API project

---
+++ AdminService_API/src/main/java/org/researchbucks/AdminService_API/AdminServiceApiApplication.java
@@ -0,0 +1,13 @@
+package org.researchbucks.AdminService_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServiceApiApplication.class, args);
+	}
+
+}

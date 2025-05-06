fd2cfa794eac0a6396ed0a1c4311b21c0fcbfc8a
Thakshila-Bandara <y.m.t.d.bandara@gmail.com>
2025-05-07 00:03:05 +0530

    feat: added service registry

--- AdminService-API/src/main/java/org/researchbucks/AdminService_API/AdminServiceApiApplication.java
+++ AdminService-API/src/main/java/org/researchbucks/AdminService_API/AdminServiceApiApplication.java
@@ -2,8 +2,10 @@ package org.researchbucks.AdminService_API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AdminServiceApiApplication {
 
 	public static void main(String[] args) {

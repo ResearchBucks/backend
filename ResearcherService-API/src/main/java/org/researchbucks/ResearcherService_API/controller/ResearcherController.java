234438012b803e8f3e8d80a137711059712a1b45
Thakshila-Bandara <y.m.t.d.bandara@gmail.com>
2025-05-07 22:21:06 +0530

    modify: modified endpoints to work with api-gateway

--- ResearcherService-API/src/main/java/org/researchbucks/ResearcherService_API/controller/ResearcherController.java
+++ ResearcherService-API/src/main/java/org/researchbucks/ResearcherService_API/controller/ResearcherController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/researcher")
+@RequestMapping("/researcher/user")
 @Slf4j
 public class ResearcherController {
 
--- ResearcherService-API/src/main/java/org/researchbucks/ResearcherService_API/controller/SurveyController.java
+++ ResearcherService-API/src/main/java/org/researchbucks/ResearcherService_API/controller/SurveyController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/survey")
+@RequestMapping("/researcher/survey")
 @Slf4j
 public class SurveyController {
 
